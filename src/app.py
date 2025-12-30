0989b21a8cc48a7281b79bb21a4dc3762fed6ddf
leonardo <l_solanoh@hotmail.com>
2025-12-30 12:21:40 -0600

    New self-hosted runners!

--- src/app.py
+++ src/app.py
@@ -35,7 +35,7 @@ def health():
 @app.route('/api/v1/hello')
 # ‘/’ URL is bound with hello_world() function.
 def hello():
-    return "Con mi burrito Sabaneroooo voy camino a Beleen :-)"
+    return "Con mi burrito Sabaneroooo voy camino a Beleen :-) --> With new helf-hosted runners"
 
 
 
