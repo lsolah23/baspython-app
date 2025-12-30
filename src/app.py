0e8250c119fc722064d27f67ffd59f655ed9c217
leonardo <l_solanoh@hotmail.com>
2025-12-29 18:29:18 -0600

    Fix shortent commit id  ::6

--- src/app.py
+++ src/app.py
@@ -35,7 +35,7 @@ def health():
 @app.route('/api/v1/hello')
 # ‘/’ URL is bound with hello_world() function.
 def hello():
-    return "Hello, thi is the test message, pre-build :-) Hellooow..."
+    return "Con mi burrito Sabaneroooo voy camino a Beleen :-)"
 
 
 
