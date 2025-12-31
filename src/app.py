f8456543b5b5fb37c24dc5645e015165958466a0
leonardo <l_solanoh@hotmail.com>
2025-12-31 16:08:54 -0600

    Adds argocd installation

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great, Leo boy!'
+        'message': 'You are doing great, Leo boy!!'
     })
 
 
