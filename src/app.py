a7d1eff9cabe5673e7b5e52434fc0338044753a6
leonardo <l_solanoh@hotmail.com>
2026-01-03 19:22:47 -0600

    CHANGED SECRET ARGOCD

--- src/app.py
+++ src/app.py
@@ -35,7 +35,7 @@ def health():
 @app.route('/api/v1/hello')
 # ‘/’ URL is bound with hello_world() function.
 def hello():
-    return "New --> With new helf-hosted runners changed again"
+    return "With new helf-hosted runners changed --> again"
 
 
 
