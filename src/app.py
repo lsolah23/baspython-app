754199ec3ab50a70592f2bc0dfa636d8fba266f4
leonardo <l_solanoh@hotmail.com>
2026-01-05 12:34:18 -0600

    try 1st

--- src/app.py
+++ src/app.py
@@ -35,7 +35,7 @@ def health():
 @app.route('/api/v1/hello')
 # ‘/’ URL is bound with hello_world() function.
 def hello():
-    return " Hello Maduro Greatings to JAIL Dude !!!--> Cambio definitivo nuevo deployment con ARGOCD !!!!"
+    return " Nicolas Maduro pledeged not guilty before the judge  !--> Cambio definitivo nuevo deployment con ARGOCD !!!!"
 
 
 
